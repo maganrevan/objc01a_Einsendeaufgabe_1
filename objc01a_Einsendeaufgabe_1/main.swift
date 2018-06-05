905d686b0388bba8cbaecde165742c88cdc006df
kruschwitzm <magnus.kruschwitz@i12.de>
2018-06-05 10:10:29 +0200

    Initial Commit

---
+++ objc01a_Einsendeaufgabe_1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  objc01a_Einsendeaufgabe_1
+//
+//  Created by Magnus Kruschwitz on 05.06.18.
+//  Copyright © 2018 Magnus Kruschwitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
